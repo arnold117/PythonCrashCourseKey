4e018c541fd8c5947951c430f37811960957ef34
arnold117 <60877940+arnold117@users.noreply.github.com>
2020-04-20 13:57:58 +0800

    上传了 第十章练习2

---
+++ Work/Chapter10/10_2/10_2.py
@@ -0,0 +1,15 @@
+filename = r'${FileDir}\learning_python.py'
+
+with open(filename) as f:
+    lines = f.readlines()
+
+for line in lines:
+    # Get rid of newline, then replace Python with c.
+    line = line.rstrip()
+    print(line.replace('Python', 'C'))
+
+"""
+* for-loop can also be:
+* for line in lines:
+*     print(line.rstrip().replace('Python', 'C'))
+"""
